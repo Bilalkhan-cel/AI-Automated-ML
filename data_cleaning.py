5e5137b2f7eb724ed150dc663ce911b3cbf40afa
Bilalkhan-cel <bilalahmedkhan6002@gmail.com>
2026-07-11 12:59:41 +0500

    defined some functions

---
+++ data_cleaning.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler , MinMaxScaler,OneHotEncoder,LabelEncoder
+
+
+def get_columns(df):
+    col=df.columns.to_list()
+    return col
+def get_num_columns(df):
+    col=df.select_dtypes(include="number").columns.to_list()
+    return col
+def get_cat_columns(df):
+    col=df.select_dtypes(include="object").columns.tolist()
+    return col
+
+def apply_onehot(df):
+    cat_col=get_cat_columns(df)
+    oh=OneHotEncoder(sparse_output=False).set_output(transform="pandas")
+    df_encoded=oh.fit_transform(df[cat_col])
+    return df_encoded
+
+    
+    
