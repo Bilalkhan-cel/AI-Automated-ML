a0738edffdc07503d98dc77fb0237c41a666b9a3
Soha Asif <sohasif17@gmail.com>
2026-07-14 19:10:02 +0500

    bye

--- visualizations.py
+++ visualizations.py
@@ -33,11 +33,11 @@ def plot_confusion_matrix(y_test, y_pred):
         annot_kws={"size": 9}
     )
 
-    plt.xlabel("Predicted", fontsize=11)
-    plt.ylabel("Actual", fontsize=11)
+    plt.xlabel("Predicted", fontsize=13)
+    plt.ylabel("Actual", fontsize=15)
     plt.title("Confusion Matrix", fontsize=13, pad=12)
-    plt.xticks(rotation=45, ha="right")
-    plt.yticks(rotation=0)
+    plt.xticks(rotation=45, ha="right",fontsize=11)
+    plt.yticks(rotation=0, fontsize=11)
     plt.tight_layout()
 
     return save_plot()
@@ -46,9 +46,9 @@ def plot_confusion_matrix(y_test, y_pred):
 def plot_actual_vs_predicted(y_test, y_pred):
     plt.figure(figsize=(5, 4))
     plt.scatter(y_test, y_pred, alpha=0.6)
-    plt.xlabel("Actual")
-    plt.ylabel("Predicted")
-    plt.title("Actual vs Predicted")
+    plt.xlabel("Actual",fontsize=13)
+    plt.ylabel("Predicted",fontsize=15)
+    plt.title("Actual vs Predicted",fontsize=13)
     return save_plot()
 
 
@@ -57,9 +57,9 @@ def plot_residuals(y_test, y_pred):
     plt.figure(figsize=(5, 4))
     plt.scatter(y_pred, residuals, alpha=0.6)
     plt.axhline(0, color="red", linestyle="--")
-    plt.xlabel("Predicted")
-    plt.ylabel("Residual")
-    plt.title("Residual Plot")
+    plt.xlabel("Predicted",fontsize=13)
+    plt.ylabel("Residual",fontsize=15)
+    plt.title("Residual Plot",fontsize=13)
     return save_plot()
 
 def plot_feature_importance(model, feature_names):
